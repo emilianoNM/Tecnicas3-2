4e9b1c408546c5d36c9f8e18b4ae5ea25c8c00ee
emiliano nava <enava@asimov.mx>
2018-09-26 08:59:55 -0500

    Arbol

---
+++ 26Septiembre2018/Arbol.py
@@ -0,0 +1,84 @@
+#Arbol.py
+class Nodo(object):
+	"""docstring for Nodo"""
+	def __init__(self, id,padre):
+		super(Nodo, self).__init__()
+		self.id = id
+		self.padre=padre
+		self.hijos=[]
+		if padre!=None:
+			padre.addHijo(self)
+
+	def addHijo(self,hijo):
+		self.hijos.append(hijo)
+		hijo.padre=self
+	def esHoja(self):
+		return len(self.hijos)==0
+	def delete(self):
+		if(self.esRaiz()):
+		   pass
+		else:
+		   padre=self.padre
+		   hijos=padre.getHijos()
+		   hijos.remove(self)
+		   self.padre=None
+
+	def esRaiz(self):
+		return self.padre==None
+	def getHijos(self):
+		return self.hijos
+	def __str__(self):
+		return self.id 
+	def __repr__(self):
+		return self.id
+
+class ArbolBinario(object):
+	"""docstring for ArbolBinario"""
+	#balanceado 
+	#Recorrido en profundidad-primero
+    #Recorrido en anchura-primero
+	def __init__(self, nodo):
+		super(ArbolBinario, self).__init__()
+		if(nodo.esRaiz()==False):
+			raise ValueError('El nodo %s no es una raiz '%(nodo))
+		self.__raiz__ = nodo
+
+	def addNodo(self,nodo):
+			aux=self.__raiz__
+			if aux.esHoja():
+				aux.addHijo(nodo)
+			else:
+				if len(aux.getHijos())!=2:
+					aux.addHijo(nodo)
+				else:
+					hijos=aux.getHijos()
+					for hijo in hijos:
+						if hijo.esHoja():
+							hijo.addHijo(nodo)
+							return 
+					for hijo in hijos:
+						if len(hijo.getHijos())!=2:
+							hijo.addHijo(nodo)
+							return
+
+
+	def __str__(self):
+			pass	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
---
+++ 26Septiembre2018/test2Arbol.py
@@ -0,0 +1,23 @@
+#test2Arbol.py
+import Arbol
+
+A=Arbol.Nodo('A',None)
+
+
+MiPrimerArbol=Arbol.ArbolBinario(A)
+#MiPrimerArbol=Arbol.ArbolBinario(B)
+MiPrimerArbol.addNodo(Arbol.Nodo('B',None))
+MiPrimerArbol.addNodo(Arbol.Nodo('C',None))
+MiPrimerArbol.addNodo(Arbol.Nodo('F',None))
+MiPrimerArbol.addNodo(Arbol.Nodo('G',None))
+MiPrimerArbol.addNodo(Arbol.Nodo('J',None))
+MiPrimerArbol.addNodo(Arbol.Nodo('K',None))
+MiPrimerArbol.addNodo(Arbol.Nodo('L',None))
+MiPrimerArbol.addNodo(Arbol.Nodo('M',None))
+print A
+for  hijo in  A.getHijos():
+	print "   ",hijo
+	for hio2hijo in hijo.getHijos():
+		print "        ",hio2hijo
+		for hio2hijo2hijo in hio2hijo.getHijos():
+			print "           ",hio2hijo2hijo
