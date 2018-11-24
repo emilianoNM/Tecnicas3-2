7f7651af67474ff83cb5fc83e2f7fc6b010a980f
GerardoBec <gerardobec9511@gmail.com>
2018-11-23 23:01:04 -0600

    Add files via upload

---
+++ Reposicion Gerardo Becerra/3 Oct/Rep 2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def es_bisiesto(t):
+    return t % 400 == 0 or (t % 100 != 0 and t % 4 == 0)
+
+print("Comprobador de anios bisiestos")
+fecha = int(input("Escriba un anio, el programa indicará su naturaleza: "))
+if es_bisiesto(fecha):
+    print(fecha, "es un año bisiesto.")
+else:
+    print(fecha, "no es un año bisiesto.")
+
