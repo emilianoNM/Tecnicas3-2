6a461fc3087fa7a2ec4c83ab7960327a7b88acfd
GerardoBec <gerardobec9511@gmail.com>
2018-11-23 23:26:04 -0600

    Add files via upload

---
+++ Reposicion Gerardo Becerra/9 Nov/Rep2.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+#Gerardo Becerra
+
+n = 1n = 1
+h = ''
+while n <= 25:
+    if n%2 != 0:
+        h += ' %i' % n
+    n += 1
+print h
+while n <= 100: 
+    print n,
+    n += 1
+
