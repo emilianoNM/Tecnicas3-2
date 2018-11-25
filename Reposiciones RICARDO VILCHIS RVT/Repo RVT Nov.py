6a860f7374d7f4dc11f9fdae59e40d41ab080aea
cbluelennon <thebluelennon@hotmail.com>
2018-11-24 23:22:24 -0600

    Add files via upload

---
+++ Reposiciones RICARDO VILCHIS RVT/Repo RVT Nov.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("funcion que calcula el perimetro de un circulo ")
+
+
+# In[ ]:
+
+
+def perimetroCirc():
+    print (":Perimetro del Circulo:")
+    tmp = int(input("Dame el perimetro del circulo: "))
+    print (" El area del circulo es: ", (3.1416) * tmp)
+
+perimetroCirc()
+
