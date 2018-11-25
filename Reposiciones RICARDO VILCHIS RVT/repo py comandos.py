f80a1fef5a3bb3824b0aa942fcbb734322c4e20d
cbluelennon <thebluelennon@hotmail.com>
2018-11-24 23:21:01 -0600

    Add files via upload

---
+++ Reposiciones RICARDO VILCHIS RVT/repo py comandos.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("POR RVT")
+print ("Este es un programa de python")
+
+
+# In[2]:
+
+
+a=("Ricardo Vilchis", 21  ,"Ing. Mecatronica","23/11/2018")
+
+
+# In[3]:
+
+
+def funcion(a):
+    print("Nombre:", a[0] )
+    print("Edad:"  , a[1] )
+    print("Carrera:" ,a[2 ])
+    print("Fecha de ingreso aprox:" , a[3])
+    
+    
+
+
+# In[4]:
+
+
+funcion(a)
+
