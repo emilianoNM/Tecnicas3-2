b6a15f9042e02cb253f95c5ba46717a7a60d729f
cbluelennon <thebluelennon@hotmail.com>
2018-11-24 23:17:39 -0600

    Add files via upload

---
+++ Reposiciones RICARDO VILCHIS RVT/Invertir cadenas y listas con recursividad.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Programa que inviete una cadena (y tambien aplica para listas) con uso de recursividad")
+print("por Ricardo Vilchis Tomás")
+
+
+# In[2]:
+
+
+def invertir(var):
+        return var[::-1]
+invertir("cadena")
+
+
+# In[3]:
+
+
+invertir([1,2,3,4,5])
+
