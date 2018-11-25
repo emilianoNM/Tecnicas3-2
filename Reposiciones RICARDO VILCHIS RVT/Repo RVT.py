8e8f7eb009a2c780c8e435548cecb537a4ba38aa
cbluelennon <thebluelennon@hotmail.com>
2018-11-24 23:14:09 -0600

    Add files via upload

---
+++ Reposiciones RICARDO VILCHIS RVT/Repo RVT.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Operaciones con listas")
+print("Por RVT")
+
+
+# In[2]:
+
+
+(/Invertir, los, elementos, de, una, lista, ya, establecida)
+empresas= ["Asimov", "Bimbo", "Apple", "PH"]
+empresas.reverse()
+print (empresas)
+
+
+# In[ ]:
+
+
+
+
