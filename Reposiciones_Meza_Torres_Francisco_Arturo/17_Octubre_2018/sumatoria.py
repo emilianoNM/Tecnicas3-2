ee17c3ebaa24045d52579bfb363775448f28f571
Francisco54321 <42306539+Francisco54321@users.noreply.github.com>
2018-11-25 13:36:09 -0600

    Add files via upload

---
+++ Reposiciones_Meza_Torres_Francisco_Arturo/17_Octubre_2018/sumatoria.py
@@ -0,0 +1,14 @@
+print("Sumatoria\n")
+a=-2
+totalq=0
+
+def suma(i,total):
+    for i in range(-2,10):
+        x= i/2
+        total= total + x
+        print(total)
+    
+
+        
+
+suma(a,totalq)
