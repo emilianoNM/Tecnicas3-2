f2c891b2e997e4df2da794ec1e03c24e6adc80ba
cbluelennon <thebluelennon@hotmail.com>
2018-11-24 23:25:08 -0600

    Add files via upload

---
+++ Caracteres cadena proto.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Prgrama que duplica caracteres de una cadena por Ricardo Vilchis")
+
+
+# In[2]:
+
+
+x="Hola"
+print("concatenare la cadena")
+y=x*2
+print(y)
+print("OTRA FORMA")
+
