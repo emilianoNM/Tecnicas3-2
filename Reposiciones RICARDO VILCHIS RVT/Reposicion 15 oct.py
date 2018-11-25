480161e241d9815e6e59fc79918ab4ea7a449cb1
cbluelennon <thebluelennon@hotmail.com>
2018-11-24 23:19:37 -0600

    Add files via upload

---
+++ Reposiciones RICARDO VILCHIS RVT/Reposicion 15 oct.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Por Ricardo Vilchis Tomás")
+
+
+# In[2]:
+
+
+n = 100
+h = 0
+while n >= 1:
+    if n%2 != 0:
+        print (n)
+        h += n
+    n -= 1
+print ('y su suma es: %i' % h)
+ 
+
