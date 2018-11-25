0e8502b0afc342079942dced72ff85b217c61011
cbluelennon <thebluelennon@hotmail.com>
2018-11-24 23:19:55 -0600

    Add files via upload

---
+++ Reposiciones RICARDO VILCHIS RVT/Reposicion 29 oct.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Por Ricardo Vilchis Tomás")
+
+
+# In[2]:
+
+
+h = int(input('Introduzca un numero: '))
+if h%2 == 0:
+    print ('Este numero es par')
+else:
+    print ('Este numero es impar')
+
