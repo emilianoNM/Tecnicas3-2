1b65a3a181965b82b2d433aa70c67a389076e235
cbluelennon <thebluelennon@hotmail.com>
2018-11-24 23:18:17 -0600

    Add files via upload

---
+++ Reposiciones RICARDO VILCHIS RVT/Programa que invierta una cadena sin usar librerias.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Programa que invierta una cadena sin uso de librerias")
+print("Por Ricardo Vilchis Tomás")
+
+
+# In[2]:
+
+
+cadena=input("Ingresa la cadena papau\n")
+
+cadena = cadena[::-1]
+
+print("aqui esta tu cadena  invertida, 800 pesos papa\n",cadena)
+
+print("sin usar librerias")
+
